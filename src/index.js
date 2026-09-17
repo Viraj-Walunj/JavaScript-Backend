49307ea53d7f0f0f0e1552118b182800d1a8c235
Viraj Walunj <virajwalunj96@gmail.com>
2026-09-17 10:06:08 +0530

    Connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// require ('dotenv').config({path: './env'})
+
+// OR(now in the current version not needed to add this)
+
+//using experimental import for code modularity(but not work until you use this in package.json
+// "scripts": {
+//     "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+//   },
+// )
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "monngoose";
+import { DB_NAME } from "./constants";
+
+// Approach 1
+import express from "express"
+const app = express()
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error)=>{
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch (error){
+        console.log("ERROR: ", error)
+        throw error
+    }
+})
+*/

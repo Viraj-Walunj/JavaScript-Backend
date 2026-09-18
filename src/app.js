56e522df32cff165c70f243ba4f21450cac98881
Viraj Walunj <virajwalunj96@gmail.com>
2026-09-18 13:47:46 +0530

    Initialize Express application settings and connect server entry point
    * Build `app.js` with security (CORS), parsing, and public static file serving middleware
    * Add `.then()` block to index.js to activate `app.listen` on port 8000 upon DB connection
    * Add `.catch()` block to log MongoDB initial connection errors

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+})) 
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static("public")) //saving file and folder and making public assets where people can access files(folder name can be any , here we have kept name as public)
+app.use(cookieParser()) //used for accessing and setting cokkies in users browser
+export {app}

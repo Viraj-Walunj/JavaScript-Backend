dd925d51dc3c11ffcafd64e61b11feae240d4230
Viraj Walunj <virajwalunj96@gmail.com>
2026-09-23 15:58:27 +0530

    feat(routes): add user registration route and controller •	Created user.controller.js with registerUser handler. •	Set up user.routes.js with POST route definition. •	Registered user routes in app.js using middleware. •	Configured async request handling for controllers.

--- src/app.js
+++ src/app.js
@@ -13,4 +13,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public")) //saving file and folder and making public assets where people can access files(folder name can be any , here we have kept name as public)
 app.use(cookieParser()) //used for accessing and setting cokkies in users browser
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)  // http://localhost:8000//api/v1/users/register
+
 export {app}
